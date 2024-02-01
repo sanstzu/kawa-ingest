b868dda43a3b79dc3ce26b09b0ed6577cb791cfa
Clayton Fernalo <clafernalo@gmail.com>
2024-02-02 02:59:49 +0800

    feat: add proto to main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,20 @@ use log::{error, info};
 use tokio::net::TcpListener;
 
 mod connection;
+mod extractor;
 mod stream_manager;
+mod transcoder;
 
 pub type BoxError = Box<dyn std::error::Error + Send + Sync>;
+pub mod proto {
+    tonic::include_proto!("kawa");
+}
 
 #[tokio::main]
 async fn main() -> Result<(), BoxError> {
     dotenv().ok();
     env_logger::init();
+
     let manager_sender = stream_manager::start();
     let connection = TcpListener::bind("0.0.0.0:1935").await?;
     info!("Listening on port 1935");
